7b9ad5d73e4203296f00ad1956e8b6cebe44b64e
lluuiissoo <lluuiissoo@gmail.com>
2016-08-05 17:07:58 -0400

    Adding MergeSort top-bottom implementation with unit tests and debug information.

---
+++ SortingAlgorithms/mergesort.sorting/readme.txt
@@ -0,0 +1,11 @@
+﻿Algorithm
+--------------
+Conceptually, a merge sort works as follows:
+- Divide the unsorted list into n sublists, each containing 1 element (a list of 1 element is considered sorted).
+- Repeatedly merge sublists to produce new sorted sublists until there is only 1 sublist remaining. This will be the sorted list.
+
+Ref: https://en.wikipedia.org/wiki/Merge_sort
+
+http://www.softwareandfinance.com/CSharp/MergeSort_Recursive.html
+
+
---
+++ SortingAlgorithms/tests.mergesort.sorting/MergeSortTests.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using NUnit.Framework;
+using mergesort.sorting;
+using System.Collections.Generic;
+
+namespace tests.mergesort.sorting
+{
+    [TestFixture]
+    public class MergeSortTests
+    {
+        private MergeSort algorithm;
+
+        [SetUp]
+        public void Setup()
+        {
+            algorithm = new MergeSort();
+        }
+
+        [Test]
+        public void MergeSort_SortedList_ReturnsSameList()
+        {
+            List<int> sortedList = new List<int> { 1, 2, 3, 4, 6 };
+            
+            var result = algorithm.Sort(sortedList);
+ 
+            Assert.That(result, Is.EqualTo(sortedList));
+        }
+
+        [Test]
+        public void MergeSort_UnsortedList_ReturnsSortedList()
+        {
+            List<int> unsortedList = new List<int> { 5, 7, 3, 4, 6, 2, 1 };
+
+            List<int> sortedList = new List<int> { 1, 2, 3, 4, 5, 6, 7 };
+
+            var result = algorithm.Sort(unsortedList);
+
+            Assert.That(result, Is.EqualTo(sortedList));
+        }
+    }
+}
